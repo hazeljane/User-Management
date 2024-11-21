46e413da9924848139e15f63e41d94c0218de08d
hazeljane <gunohazeljane@gmail.com>
2024-11-21 10:16:55 +0700

    Update userModel.ts

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
 import { IUser } from "../interfaces/userInterface";  // Assuming you have an IUser interface
 
 // Define the schema for the User model
@@ -16,11 +17,21 @@ export const userSchema: Schema = new Schema(
     
     // Last name field - required and max 50 characters
     lastName: { type: String, required: true, maxlength: 50 },
-
+    
+    //Optional fields (could be useful in a social media application)
+    dateOfBirth: { type: Date },
+    contactNumber: { type: String },
+    following: { type: [String],default: [] }, //Array of user IDs this user is following
+    
     // Enable automatic timestamp fields (createdAt and updatedAt)
   },
   { timestamps: true }
 );
+
+// Instance method to compare entered password with stored hashed password
+userSchema.methods.isValidPassword = async function ( password: string ): Promise<boolean>{
+return bcrypt.compare(password, this password);
+};
 
 // Create and export the User model
 export const User = mongoose.model<IUser>("User", userSchema);
